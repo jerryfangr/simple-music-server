b048b5e4c66316c79df648814979192be133e83d
jerryfangr <2a15068517423@gmail.com>
2021-04-01 17:24:42 +0800

    remove some check

--- tools/json-database.js
+++ tools/json-database.js
@@ -1,32 +1,36 @@
 const path = require('path');
 var fs = require('fs');
-const { all } = require('../routes');
 
+/**
+ * * an json database
+ * * it should start width fetAll function, all functions can repeat and change the order
+ * * ex: new JsonDatabase('dbname').fetchAll().limit(1,2).remove(1).limit(0,1).get(data => {}).fetAll()...
+ */
 class JsonDatabase {
   constructor(databaseName) {
-    this.dbName = databaseName;
     this.dbPath = path.join('db/json', databaseName + '.json');
     this._state = 'init';
-    this._content = undefined;
-    this._stack = Promise.resolve();
+    this._stack = Promise.resolve([]);
+  }
+
+  toString() {
+    let status = 'db file: ' + this.dbPath;
+    status += '\nstate: ' + this._state;
+    return status;
   }
 
   /**
    * * read file content and convert to string;
    */
   _readContent () {
-    const promise =  new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       fs.readFile(this.dbPath, function (err, data) {
         if (err) { reject(err) };
         resolve(data?.toString?.())
       });
     }).then((data) => {
-      data = data || '[]';
-      this._content = data;
-    }, error => {
-      this._content = '[]';
+      return JSON.parse(data || '[]')
     });
-    return promise;
   }
 
   /**
@@ -36,60 +40,32 @@ class JsonDatabase {
   _writeContent(content) {
     return new Promise((resolve, reject) => {
       fs.writeFile(this.dbPath, content, {flag: 'w', encoding: 'utf-8'}, function (err) {
-        if (err) { 
-          console.log('error happen');
-          reject(err); 
-        }
+        if (err) { reject(err); }
         resolve()
       });
     });
   }
 
-  _reset() {
-    this._state = 'init';
-    this._content = undefined;
-    this._stack = Promise.resolve();
-  }
-
-  _checkContent () {
-    if (this._state === 'init') {
-      this.fetchAll();
-    }
-  }
-
-  toString () {
-    return this._content;
-  }
-
-  toObject () {
-    return JSON.parse(this._content);
-  }
-
   /**
-   * * push fetch data to stack
-   * @returns 
+   * * fetch all data
    */
   fetchAll () {
-    if (this._state === 'end') {
-      this._reset();
-    }
-    this._state = 'fetchAll';
-    this._stack = this._stack.then(() => {
+    this._state = 'select';
+    this._stack = Promise.resolve().then(() => {
       return this._readContent();
-    }).then(() => {
-      return this.toObject();
-    });
+    })
     return this;
   }
 
   /**
-   * * push get limit data to stack
+   * * limit data 
    * @param {number} startIndex start index
    * @param {number} number number of data
-   * @returns 
    */
   limit(startIndex, number) {
-    this._checkContent();
+    if (this._state === 'init') {
+      throw new Error('Not allowed to start width limit funtion')
+    }
     this._stack = this._stack.then(allDatas => {
       const endIndex = startIndex + (number || allDatas.length) - 1;
       const result = [];
@@ -104,17 +80,22 @@ class JsonDatabase {
   }
 
   /**
-   * filte data
-   * @param {Object} options 
-   * @returns 
+   * * filter data
+   * @param {Object} options ex: { name: 'name',  age: /\d{1,2}/}
    */
   filter (options) {
-    this._checkContent();
+    if (this._state === 'init') {
+      throw new Error('Not allowed to start width filter funtion')
+    }
     if (options.id) { options.id = parseInt(options.id, 10);}
     this._stack = this._stack.then(allDatas => {
       return allDatas.filter(d => {
         for (const key in options) {
-          if (options[key] !== d[key]) {
+          if (options[key] instanceof RegExp) {
+            if (!options[key].test(d[key])) {
+              return false;
+            }
+          } else if (options[key] !== d[key]) {
             return false;
           }
         }
@@ -126,71 +107,100 @@ class JsonDatabase {
 
   /**
    * * get data by callback
-   * @param {Function} callback 
-   * @param {Function} onReject 
-   * @returns 
+   * @param {Function} success
+   * @param {Function} fail
    */
   get (success, fail) {
-    this._checkContent();
-    this._state = 'end';
-    this._stack.then(success, fail);
+    this._stack.then(data => {
+      success(data);
+      return data;
+    }, fail);
     return this;
   }
 
-  _edit(action, success, fail) {
-    this.fetchAll();
-    this._stack.then(allDatas => {
-      let newData = action(allDatas);
-      return this._writeContent(JSON.stringify(newData));
-    }).then(success, fail);
+  /**
+   * * edit data
+   * @param {Function} action edit function
+   * @param {Boolean} force force do the action whitout select
+   */
+  _edit(action, force) {
+    if (this._state !== 'edit' && !force && this._state !== 'select') {
+      throw new Error('You have to select some data fetAll(), or set param force true');
+    }
+    this._state = 'edit';
+    this._stack = this._stack.then(allDatas => {
+      return action(allDatas);
+    });
 
     return this;
   }
 
   /**
-   * * add data to file
-   * @param {Object} data 
-   * @param {Function} callback 
+   * * add data
+   * @param {Object} data
+   * @param {Boolean} force force do this function
    */
-  add(data, success, fail) {
-    this._edit(allDatas => {
+  add(data, force) {
+    return this._edit(allDatas => {
       const lastData = allDatas[allDatas.length - 1] || {id: -1};
       data.id = lastData.id + 1;
       allDatas.push(data);
       return allDatas;
-    }, success, fail)
+    }, force);
   }
 
   /**
    * * remove data by data id
    * @param {number} id 
-   * @param {Function} callback 
+   * @param {Boolean} force force do this function
    */
-  remove(id, success, fail) {
+  remove(id, force) {
     id = parseInt(id, 10);
-    this._edit(allDatas => {
+    return this._edit(allDatas => {
       return allDatas.filter(d => d.id !== id);
-    }, success, fail)
+    }, force);
   }
 
   /**
    * * update data which has same id
    * @param {Object} newData 
-   * @param {Function} callback
+   * @param {Boolean} force force do this function
    */
-  update(newData, success, fail) {
-    newData.id = parseInt(newData.id, 10);
-    this._edit(allDatas => {
+  update(data, force) {
+    data.id = parseInt(data.id, 10);
+    return this._edit(allDatas => {
       for (let i = 0; i < allDatas.length; i++) {
-        if (allDatas[i].id === newData.id) {
-          for (const key in newData) {
-            allDatas[i][key] = newData[key];
+        if (allDatas[i].id === data.id) {
+          for (const key in data) {
+            allDatas[i][key] = data[key];
           }
           break;
         }
       }
       return allDatas;
-    }, success, fail);
+    }, force);
+  }
+
+  /**
+   * * overwrite the data(which in stack) to json database
+   * @param {Function} success 
+   * @param {Function} fail
+   * @param {Boolean} force force do this function, it may clear all data
+   */
+  send(force, success, fail) {
+    if (this._state === 'send') { return; }
+    if (!force && this._state === 'init') {
+      throw new Error('Not allowed to start width send funtion, or set param force true(clear all data)')
+    }
+
+    this._state = 'send';
+    this._stack.then(allDatas => {
+      return this._writeContent(JSON.stringify(allDatas));
+    }).then(data => {
+      success && success(data);
+      return data;
+    }, fail);
+    return this;
   }
 }
 
