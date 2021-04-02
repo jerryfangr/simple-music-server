d2104875682672f0cd17a05620f874bc494cd041
jerryfangr <2a15068517423@gmail.com>
2021-04-02 18:47:44 +0800

    add entry file

--- public/uploader/src/uploader/index.js
+++ public/uploader/src/uploader/index.js
@@ -1,12 +1,15 @@
 import './index.less';
-// import uploader from './song-uploader';
-// import songForm from './song-form';
-// import songCreator from './song-creator';
-// import songList from './song-list';
+import uploader from './components/upload-box/upload-box';
 
+console.log('uploader', uploader);
+uploader.init()
 // songForm.defaultInit()
 // songCreator.defaultInit()
 // songList.defaultInit()
 // uploader.defaultInit()
 
+// import uploader from './song-uploader';
+// import songForm from './song-form';
+// import songCreator from './song-creator';
+// import songList from './song-list';
 
